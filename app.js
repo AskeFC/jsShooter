1845aadbeea009f44eeeb8052c46af2b31201a07
Aske Færch Christensen <askefc@gmail.com>
2023-07-03 20:50:40 +0200

    Test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { Server } from 'socket.io';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import colors from 'colors/safe';
-import middleware from 'socketio-wildcard';
+const middleware = require('socketio-wildcard');
 
 const app = express();
 const serv = createServer(app);
